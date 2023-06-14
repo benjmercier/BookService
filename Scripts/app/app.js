c1dc131472c1b2a5bd61dbd5aa06bc386a8845bd
Ben <benjmercier11@gmail.com>
2023-06-14 14:48:11 -0400

    Added knockout.js to the project, created an indexVM view model, and applied bindings in app.js.

---
+++ Scripts/app/app.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="../knockout-3.5.1.js" />
+
+import IndexVM from "./viewmodels/indexVM.js";
+
+// The main entry point into the client side application
+
+let index = IndexVM();
+
+// fetches initial data
+index.getAllBooks();
+
+ko.applyBindings(index);
